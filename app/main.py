1a886d7d8bd33ed819942ac43791cbd15bb2fdba
C113156104 <C113156104@nkust.edu.tw>
2025-11-29 17:45:56 +0800

    新增前端畫面

--- app/main.py
+++ app/main.py
@@ -1,30 +1,36 @@
+import os
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 from app.database import Base, engine
 from app.routers import expenses
 from app.config import settings
 
-
 def create_app() -> FastAPI:
     app = FastAPI(title=settings.PROJECT_NAME)
 
-    # 自動建立資料表（真實專案用 Alembic）
+    # 自動建立資料表
     Base.metadata.create_all(bind=engine)
 
+    # 掛載靜態檔案 (讓網頁可以讀取 CSS/JS)
+    # 確保 static 資料夾存在
+    if not os.path.exists("static"):
+        os.makedirs("static")
+        
+    app.mount("/static", StaticFiles(directory="static"), name="static")
+
     # 掛載 Router
+    # 注意：因為有加 prefix="/api/v1"，所以 API 路徑變成了 /api/v1/expenses
     app.include_router(expenses.router, prefix="/api/v1")
 
+    # 修改首頁路由
     @app.get("/")
     def root():
-        return {"message": "System is running. Go to /docs for API."}
+        # 當使用者打開首頁時，回傳前端網頁
+        return FileResponse('static/index.html')
 
     return app
 
-
 app = create_app()
 
-
-'''
-啟動在終端機輸入:uvicorn app.main:app --reload
-
-expense的意思是 支出/開銷，這是記帳系統所以變數都用expense
-'''
+#啟動在終端機輸入:uvicorn app.main:app --reload
